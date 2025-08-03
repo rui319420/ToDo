35f9f05d1b15d8f0f6f328e2023be7a564aa94a1
Rui <s1F102501866@iniad.org>
2025-08-04 02:00:48 +0900

    最初のコミット

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,25 @@ import styles from './page.module.css';
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>ToDoリスト</h1>
         <div>
-          <input className={styles.input} type="text" placeholder="新しいToDo" />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="新しいToDo"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
           <button className={styles.addButton}>追加</button>
         </div>
+        <p>現在の入力値: {inputValue}</p>
       </main>
     </div>
   );
